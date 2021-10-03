74d20d73157fff0d5f360f0e8a9f9f60a1eab259
Zivanai A. Doma <zivanaiarnolddoma@gmail.com>
2021-10-03 15:59:29 +0300

    Add save function to save  text file

--- js/gCipher.js
+++ js/gCipher.js
@@ -50,6 +50,23 @@ const gCipher = function(text, key, func, printAll = true ){
         plain text: ${text}
         
         transformed text: ${transformedText.join("")}`)
+
+        function save(filename, data) {
+          const blob = new Blob([data], {type: 'text/csv'});
+          if(window.navigator.msSaveOrOpenBlob) {
+            window.navigator.msSaveBlob(blob, filename);
+          }
+          else{
+            const elem = window.document.createElement('a');
+            elem.href = window.URL.createObjectURL(blob);
+            elem.download = filename;        
+            document.body.appendChild(elem);
+            elem.click();        
+            document.body.removeChild(elem);
+          }
+        }
+        save('newFile.txt', transformedText);
+
     }
     return (transformedText.join(""))
 };
